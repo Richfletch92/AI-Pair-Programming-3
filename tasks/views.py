4093ba9bd63071614f0520b7ffd8172a3c142524
Richfletch92 <richfletch92@gmail.com>
2025-01-21 15:32:31 +0000

    create view and intial page layout for task list

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Task
 
-# Create your views here.
+def task_list(request):
+    todo_tasks = Task.objects.filter(completed=False).order_by('-due_date')
+    done_tasks = Task.objects.filter(completed=True).order_by('-due_date')
+    context = {
+        'todo_tasks': todo_tasks,
+        'done_tasks': done_tasks,
+    }
+    return render(request, 'index.html', context)
